98d5f8f5061215ec049795ef53bd5742733fa878
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-14 15:54:02 -0400

    cars router set up no endpoints. db config built

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const carsRouter = require('../cars/carsRouter')
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/cars', carsRouter)
 server.get('/', (req, res)=>{
     res.send(`<h1>LandoDrives API</h1>`)
 })
