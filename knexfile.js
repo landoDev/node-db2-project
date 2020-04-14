50f1cf717eb6a29be56bdc312c383742921851b1
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-14 15:43:35 -0400

    knexfile fleshed out

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,14 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
