98577dd6579bd2bbfb9589c383c8eaf3ebbe2ee3
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-14 16:38:59 -0400

    adds endpoint and empty db

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const carsRouter = require('../cars/carsRouter')
+const carsRouter = require('../cars/carsRouter');
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/cars', carsRouter)
+server.use('/api/cars', carsRouter);
 server.get('/', (req, res)=>{
-    res.send(`<h1>LandoDrives API</h1>`)
-})
+    res.send(`<h1>LandoDrives API</h1>`);
+});
 
 module.exports = server;
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
+const db = require('../data/db-config')
 const router = express.Router();
 
+router.get('/', (req, res) =>{
+    // get end point
+    db('*').from('cars')
+    db('cars')
+    .then(cars =>{
+        res.status(200).json(cars)
+    })
+    .catch(err=>{
+        res.status(500).json({ error: 'Could not get cars'})
+    })
+    
+})
 
 module.exports= router;
